6332dcdf7bcee3edef1abfdae6eae2d0b0296e43
Ömür Uçum <omurucum@gmail.com>
2023-09-16 03:00:26 +0300

    refactor Content View

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -3,31 +3,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            LinearGradient(
-                    gradient: Gradient(colors: [.blue, Color("lightBlue")]),
-                    startPoint: .topLeading,
-                    endPoint: .bottomTrailing)
-                .edgesIgnoringSafeArea(.all)
+            BackgroudView(topColor: .blue, bottomColor: Color("lightBlue"))
             VStack{
                 
-                Text("İstanbul, TR")
-                    .font(.system(size:32, weight: .medium, design: .default))
-                    .foregroundColor(.white)
-                    .padding()
+                CityTextView(cityName: "İstanbul, TR")
                 
-                VStack(spacing: 10) {
-                    
-                    Image(systemName: "cloud.sun.fill")
-                        .renderingMode(.original)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 180, height: 180)
-                    
-                    Text("76°")
-                        .font(.system(size: 70, weight: .medium))
-                        .foregroundColor(.white)
-                }
-                .padding(.bottom, 40)
+                CurrentTemperature(imageName: "cloud.sun.fill", temperature: 76)
                 
                 HStack(spacing: 20){
                     WeatherDayView(dayOfWeek: "TUE", imageName: "sun.max.fill", temperature: 76)
@@ -41,11 +22,7 @@ struct ContentView: View {
                 Button{
                     print("button tapped")
                 } label: {
-                    Text("Change Day Time")
-                        .frame(width: 280, height:50)
-                        .background(.white)
-                        .font(.system(size: 20, weight: .bold, design: .default))
-                        .cornerRadius(3.0)
+                    WeatherButton(title: "Change Day Time", bgColor: .white, textColor: .blue)
                 }
                 
                 Spacer()
@@ -55,7 +32,7 @@ struct ContentView: View {
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View { 
+    static var previews: some View {
         ContentView()
     }
 }
@@ -85,3 +62,67 @@ struct WeatherDayView: View{
     }
 }
 
+
+struct BackgroudView: View {
+    
+    var topColor: Color
+    var bottomColor: Color
+    
+    var body: some View {
+        LinearGradient(
+            gradient: Gradient(colors: [topColor, bottomColor]),
+            startPoint: .topLeading,
+            endPoint: .bottomTrailing)
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct CityTextView: View {
+    
+    var cityName: String
+    
+    var body: some View{
+        Text(cityName)
+            .font(.system(size:32, weight: .medium, design: .default))
+            .foregroundColor(.white)
+            .padding()
+    }
+}
+
+struct CurrentTemperature: View{
+    
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View{
+        VStack(spacing: 10) {
+            
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 180, height: 180)
+            
+            Text("\(temperature)°")
+                .font(.system(size: 70, weight: .medium))
+                .foregroundColor(.white)
+        }
+        .padding(.bottom, 40)
+    }
+}
+
+struct WeatherButton: View{
+    
+    var title: String
+    var bgColor: Color
+    var textColor: Color
+    
+    var body: some View{
+        Text(title)
+            .frame(width: 280, height:50)
+            .background(bgColor)
+            .foregroundColor(textColor)
+            .font(.system(size: 20, weight: .bold, design: .default))
+            .cornerRadius(5.0)
+    }
+}
