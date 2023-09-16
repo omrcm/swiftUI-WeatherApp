7e0372795540233f22c01f9835282885ac2b1cfd
Ömür Uçum <omurucum@gmail.com>
2023-09-16 03:46:54 +0300

    refactor code

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -2,14 +2,16 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State private var isNight = false
+    
     var body: some View {
         ZStack {
-            BackgroudView(topColor: .blue, bottomColor: Color("lightBlue"))
+            BackgroudView(isNight: isNight)
             VStack{
                 
                 CityTextView(cityName: "İstanbul, TR")
                 
-                CurrentTemperature(imageName: "cloud.sun.fill", temperature: 76)
+                CurrentTemperature(imageName: isNight ? "moon.stars.fill" : "cloud.sun.fill", temperature: 76)
                 
                 HStack(spacing: 20){
                     WeatherDayView(dayOfWeek: "TUE", imageName: "sun.max.fill", temperature: 76)
@@ -21,7 +23,7 @@ struct ContentView: View {
                 Spacer()
                 
                 Button{
-                    print("button tapped")
+                    isNight.toggle()
                 } label: {
                     WeatherButton(title: "Change Day Time", bgColor: .white, textColor: .blue)
                 }
@@ -51,7 +53,7 @@ struct WeatherDayView: View{
                 .foregroundColor(.white)
             
             Image(systemName: imageName)
-                .renderingMode(.original)
+                .symbolRenderingMode(.multicolor)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 40, height: 40)
@@ -66,12 +68,11 @@ struct WeatherDayView: View{
 
 struct BackgroudView: View {
     
-    var topColor: Color
-    var bottomColor: Color
+    var isNight: Bool
     
     var body: some View {
         LinearGradient(
-            gradient: Gradient(colors: [topColor, bottomColor]),
+            gradient: Gradient(colors: [isNight ? .black : .blue, isNight ? .gray : Color("lightBlue")]),
             startPoint: .topLeading,
             endPoint: .bottomTrailing)
         .edgesIgnoringSafeArea(.all)
